998e1828c4c727bece38eb05747bd17298f884fc
ZRoman87 <zrazhevskiy.roman@yandex.ru>
2023-05-29 13:14:09 +0300

    1st commit

---
+++ src/main/java/com/example/skyprocoursework2/SkyproCoursework2Application.java
@@ -0,0 +1,13 @@
+package com.example.skyprocoursework2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproCoursework2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproCoursework2Application.class, args);
+	}
+
+}
